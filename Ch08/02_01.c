c2b6a0526604386992c65d1432cb9b7a18e32c43
2017029834 <spes2you@gmail.com>
2018-01-07 23:24:24 -0800

    Ch08

--- Ch08/02_01.c
+++ Ch08/02_01.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 
 int main(void){
-
+	int dan=1, num=1;
+	for(int dan=1; dan<=8; dan++){
+		if(dan%2!=0)continue;
+		else{
+			for(int num =1; num<=dan; num++){
+			if(num>dan)break;
+			else{
+			printf("%d * %d = %d\n",dan,num,dan*num);
+			}
+			}
+		}
+	}
+	return 0;
+}
