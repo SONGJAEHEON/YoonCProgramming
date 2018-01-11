e6b252fd4cc074ee46ce423a3423c56d9c074e77
2017029834 <spes2you@gmail.com>
2018-01-11 07:27:35 -0800

    YoonCProgramming

---
+++ Ch09/02_01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int AddToTotal(int num){
+	static int total;
+	total +=num;
+	return total;
+}
+
+int main(void){
+	int num, i;
+	for(i=0;i<3;i++){
+		printf("insert%d: ", i+1);
+		scanf("%d", &num);
+		printf("sum: %d \n",AddToTotal(num));
+	}
+	return 0;
+}
